f005ddd9c32038ff12add793ea58d2388b5b9417
Andy Edzo <andysteph21@gmail.com>
2026-03-07 07:40:35 -0500

    feat: refactor auth

--- services/restaurant.service.ts
+++ services/restaurant.service.ts
@@ -10,12 +10,12 @@ function getSupabase() {
   if (typeof window !== "undefined") {
     return createBrowserClient(
       process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+      process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
     )
   }
   return createSupabaseRawClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
   )
 }
 
